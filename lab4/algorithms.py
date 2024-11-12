346ff41d0f14d4556d6d208e469ac1fd7e5527fd
57459N <stasun008@gmail.com>
2024-11-12 23:31:52 +0300

    prime factors for whatever...

--- lab4/algorithms.py
+++ lab4/algorithms.py
@@ -102,3 +102,20 @@ def generate_big_prime(n_bits: int):
         p = random.randint(2 ** (n_bits - 1), 2 ** n_bits - 1)
         if p % 2 == 1 and is_prime(p):
             return p
+
+
+def get_prime_factors(n):
+    factors = []
+    while n % 2 == 0:
+        factors.append(2)
+        n //= 2
+
+    for i in range(3, int(n ** 0.5) + 1, 2):
+        while n % i == 0:
+            factors.append(i)
+            n //= i
+
+    if n > 2:
+        factors.append(n)
+
+    return factors
